2c46ba040fca938c7285f807a1198efa26363d8c
M5-Vincent <m5-vincent@M5-VincentdeMacBook-Pro.local>
2026-05-15 10:44:37 +0800

    add

--- components/m5unit_encoder/m5unit_encoder.cpp
+++ components/m5unit_encoder/m5unit_encoder.cpp
@@ -55,6 +55,30 @@ void M5UnitEncoder::set_led_color(uint8_t index, uint32_t color) {
   }
 }
 
+void M5UnitEncoder::set_led_channel(uint8_t led_index, LedColorChannel channel, float state) {
+  if (led_index > 1)
+    return;
+
+  if (state < 0.0f)
+    state = 0.0f;
+  if (state > 1.0f)
+    state = 1.0f;
+
+  this->led_color_[led_index][channel] = (uint8_t)(state * 255.0f);
+  this->write_led_color_(led_index);
+}
+
+void M5UnitEncoder::write_led_color_(uint8_t index) {
+  uint8_t data[4];
+  data[0] = index;
+  data[1] = this->led_color_[index][LED_RED];
+  data[2] = this->led_color_[index][LED_GREEN];
+  data[3] = this->led_color_[index][LED_BLUE];
+  if (!this->write_bytes(RGB_LED_REG, data, 4)) {
+    ESP_LOGW(TAG, "Failed to write LED color for index %u", index);
+  }
+}
+
 void M5UnitEncoder::set_work_mode(uint8_t mode) {
   if (!this->write_byte(MODE_REG, mode)) {
     ESP_LOGW(TAG, "Failed to set work mode");
